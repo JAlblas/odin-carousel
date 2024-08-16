50098674fee06f79aff5855458cd9aebcd674554
Jasper Alblas <just_jasper@hotmail.com>
2024-08-16 22:54:39 +0200

    POC version of carousel

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import "./style.css";
 
-const dropdownMenus = document.querySelectorAll(".dropdown-menu");
-const dropdownButtons = document.querySelectorAll(".dropdown-button");
+const images = document.querySelectorAll("img");
 
-dropdownButtons.forEach((button) => {
-  button.addEventListener("mouseenter", () => {
-    const dropdownContent =
-      button.parentElement.querySelector(".dropdown-content");
-    dropdownContent.classList.toggle("hidden");
-  });
+images.forEach((image) => {
+  const index = parseInt(image.dataset.index);
+  let currentLeft = parseInt(image.style.left) || 0;
+  image.style.left = currentLeft + index * 100 + "px";
 });
 
-dropdownMenus.forEach((menu) => {
-  console.log(menu);
-  menu.addEventListener("mouseleave", () => {
-    const content = menu.querySelector(".dropdown-content");
-    content.classList.toggle("hidden");
-  });
-});
+const slides = document.querySelector(".slides");
+
+let slide = 0;
+
+setInterval(function () {
+  //let currentLeft = parseInt(slides.style.left) || 0;
+  slides.style.left = 0 - slide * 500 + "px";
+
+  if (slide > 4) {
+    slide = 0;
+    //currentLeft = 0;
+    slides.style.left = "0px";
+  } else {
+    slide++;
+  }
+
+  console.log(slides.style.left);
+}, 2000);
