9352c7119ec15d379a9c70899e4afa5c1ccbfd31
Jasper Alblas <just_jasper@hotmail.com>
2024-08-19 21:28:09 +0200

    Adds functionality to slide forward/backwards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import "./style.css";
 
 const images = document.querySelectorAll("img");
+const backButton = document.querySelector(".back");
+const nextButton = document.querySelector(".next");
+
+backButton.addEventListener("click", () => {
+  setSlide(slide - 1);
+});
+
+nextButton.addEventListener("click", () => {
+  setSlide(slide + 1);
+});
 
 images.forEach((image) => {
   const index = parseInt(image.dataset.index);
@@ -13,16 +23,16 @@ const slides = document.querySelector(".slides");
 let slide = 0;
 
 setInterval(function () {
-  //let currentLeft = parseInt(slides.style.left) || 0;
-  slides.style.left = 0 - slide * 500 + "px";
+  setSlide(slide);
+}, 2000);
 
-  if (slide > 4) {
+function setSlide(index) {
+  slides.style.left = 0 - index * 500 + "px";
+
+  if (index > 4) {
     slide = 0;
-    //currentLeft = 0;
     slides.style.left = "0px";
   } else {
     slide++;
   }
-
-  console.log(slides.style.left);
-}, 2000);
+}
